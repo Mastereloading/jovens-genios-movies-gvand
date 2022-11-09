fcdbf44ff03966b5b178627459caef309c404750
Mastereloading <guilherme-lamon@hotmail.com>
2022-11-09 18:19:51 -0300

    Fixed apollo connection

--- src/lib/neo4j/index.js
+++ src/lib/neo4j/index.js
@@ -1,65 +1,66 @@
-// import { makeAugmentedSchema } from 'neo4j-graphql-js'
 import neo4j from 'neo4j-driver'
+import { makeAugmentedSchema } from 'neo4j-graphql-js'
+import { ApolloServer } from 'apollo-server'
+import { neo4jgraphql } from 'neo4j-graphql-js'
 
-// const typeDefs = `
-//     type Movie {
-//         title: String
-//         year: Int
-//     }
-// `
-
-// const schema = makeAugmentedSchema({ typeDefs })
-
-const driver = neo4j.driver(
-  'serverLink',
-  neo4j.auth.basic('neo4j', 'deployments-stakes-disk'), 
-  {encrypted: 'ENCRYPTION_OFF'}
-)
-
-const runServer = async () => {
-  const query =
-  `
-  MATCH (n)
-  RETURN (n)
+const runServer = () => {
+  const typeDefs = `
+  type Movie {
+      title: String
+      year: Int
+      imdbRating: Float
+      genres: [Genre] @relation(name: "IN_GENRE", direction: OUT)
+  }
+  type Genre {
+      name: String
+      movies: [Movie] @relation(name: "IN_GENRE", direction: IN)
+  }
   `
 
-  // const params = {"limit": 10}
-  const session = driver.session({database:"neo4j"})
-  const data = await session.run(query) //, params)
+  const schema = makeAugmentedSchema({ typeDefs })
 
-  console.log(data.records)
+  const driver = neo4j.driver(
+    'localhost:7687',
+    neo4j.auth.basic('neo4j', 'letmein')
+  )
 
-  session.close()
-  driver.close()
+  const server = new ApolloServer({ schema, context: { driver } })
 
-    // .then((result) => {
-    //   result.records.forEach((record) => {
-    //       console.log(record.get('count'));
-    //   });
-    //   session.close();
-    //   driver.close();
-    // })
-    // .catch((error) => {
-    //   console.error(error);
-    // });
+  server.listen(3003, '0.0.0.0').then(({ url }) => {
+    console.log(`GraphQL API ready at ${url}`)
+  })
+
+  const resolvers = {
+    Query: {
+      Movie(object, params, ctx, resolveInfo) {
+        return neo4jgraphql(object, params, ctx, resolveInfo)
+      }
+    }
+  }
 }
 
 runServer()
 
-// import { ApolloServer } from 'apollo-server'
+// curl --request POST \
+//   --header 'content-type: application/json' \
+//   --url http://localhost:3003/ \
+//   --data '{"query":"query { __typename }"}'
 
-// const server = new ApolloServer({ schema, context: { driver } })
 
-// server.listen(3003, '0.0.0.0').then(({ url }) => {
-//   console.log(`GraphQL API ready at ${url}`);
-// })
-
-// import { neo4jgraphql } from 'neo4j-graphql-js'
-
-// const resolvers = {
-//   Query: {
-//     Movie(object, params, ctx, resolveInfo) {
-//       return neo4jgraphql(object, params, ctx, resolveInfo);
+// mutation {
+//   createMovies(
+//     input: [
+//       {
+//         title: "Forrest Gump"
+//         actors: { create: [{ node: { name: "Tom Hanks" } }] }
+//       }
+//     ]
+//   ) {
+//     movies {
+//       title
+//       actors {
+//         name
+//       }
 //     }
 //   }
-// };
+// }
