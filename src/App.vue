21f934966f377f470e7987236a4f91fc40a4cc8c
Mastereloading <guilherme-lamon@hotmail.com>
2022-11-12 15:47:40 -0300

    Vuetify installed

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
-<script setup lang="ts">
-import { RouterView, RouterLink } from 'vue-router'
-</script>
-
 <template>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-    <RouterView />
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,16 @@
 <template>
+  <v-avatar
+    color="primary"
+    size="56"
+  >MJG</v-avatar>
   <h1>Home</h1>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView'
+})
+
+</script>
